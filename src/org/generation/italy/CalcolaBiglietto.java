10aecdf6fdd9019e8fb2af2830afc6af68d9becb
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2021-11-16 17:01:27 +0100

    aggiunta descrizione finale

--- src/org/generation/italy/CalcolaBiglietto.java
+++ src/org/generation/italy/CalcolaBiglietto.java
@@ -13,17 +13,32 @@ public class CalcolaBiglietto {
 		double kmDaPercorrere = scanner.nextDouble();
 		System.out.print("inserisci la tua età ");
 		int eta = scanner.nextInt();
+		double prezzoPerKilometro = 0.21;
+		double scontoOver= 0.6;
+		double scontoMinorenni= 0.8;
 		
 		
-		double prezzoBase = (kmDaPercorrere * 0.21);
-		if ( eta < 18) {
-			double prezzoFinal = prezzoBase * 0.8;
-			//System.out.println(prezzoFinal);//
-		}else if (eta > 65) {
-			double prezzoFinal = prezzoBase * 0.6;
-			//System.out.println(prezzoFinal);//
+		double prezzoFinal = (kmDaPercorrere * prezzoPerKilometro);
+		if ( eta < 18 && !(eta<0)) {
+			prezzoFinal = prezzoFinal * scontoMinorenni;
 			
-		} else System.out.println(prezzoBase);
+		}else if (eta > 65) {
+			prezzoFinal = prezzoFinal * scontoOver;
+			
+			
+		}
+		System.out.println("il prezzo del biglietto è " + prezzoFinal + "€");
+		System.out.println("Età: " + eta);
+		System.out.println("Kilometri: " + kmDaPercorrere);
+		if (eta < 18 && eta>0 || eta > 65  ) {
+			System.out.print("il tuo sconto è del ");
+		}
+		if (eta < 18 && eta>0) {
+			System.out.print("20%");
+		}
+		if (eta > 65) {
+			System.out.print("40%");
+		}
 	}
 
 }
